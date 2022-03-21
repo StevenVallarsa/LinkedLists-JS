48c90973ab0462cdaceb8ad92f6ddafc803083f5
StevenVallarsa <orionsjv@gmail.com>
2022-03-21 13:43:46 -0400

    Add to head of linked list

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("HERE");
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  printList() {
+    let result = "<HEAD> ";
+    let nextNode = this.head;
+    while (nextNode) {
+      result += nextNode.data + " ";
+      nextNode = nextNode.next;
+    }
+    result += "<TAIL>";
+    console.log(result);
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    console.log(newHead);
+    if (this.head) {
+      newHead.next = this.head;
+    }
+    this.head = newHead;
+  }
+}
+
+let linkedList = new LinkedList();
+linkedList.addToHead("One");
+linkedList.addToHead("Two");
+linkedList.addToHead("Three");
+linkedList.printList();
